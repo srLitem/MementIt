b869fde509f2870caf9584d41bc5d2fa8f328493
Santiago Rhenals <santiagorhenals@gmail.com>
2020-03-08 18:27:48 +0100

    UI Improvs

--- lib/widgets/memento/memento.dart
+++ lib/widgets/memento/memento.dart
@@ -13,17 +13,25 @@ class Memento extends  StatelessWidget {
       child: Column(
         children: <Widget>[
           ListTile(
-            title: Text(title),
+            title: Text(title,
+            style: TextStyle(
+              fontFamily: 'Sans',
+              fontWeight: FontWeight.bold,
+              color: Color.fromRGBO(78, 114, 181, 1),
+            ),),
             subtitle: Text(
               'Test',
               style: TextStyle(
-                color: Colors.grey,
+                color: Color.fromRGBO(78, 114, 181, .9),
+                fontFamily: 'Sans',
+                fontStyle: FontStyle.italic,
               ),
               ),
             trailing: Text(
               '03-04-2019',
               style: TextStyle(
-                color: Colors.grey,
+                color: Color.fromRGBO(78, 114, 181, .9),
+                fontFamily: 'Sans',
               ),
               ),
             ),
